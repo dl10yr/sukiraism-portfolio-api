f6314b629b107f8b649bd62a4d487a359013a212
MacbookAir <macbookair@MacbookAir-no-MacBook-Air.local>
2020-02-07 05:31:59 +0900

    200207

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(2..100).each do |i|
+  User.create!(nickname: "test#{i}",name: "test#{i}",email: "test#{i}@example.com",password: "password", password_confirmation: "password")
+end
+
+(1..100).each do |i|
+  Post.create!(user_id: 1, content: "testtesttest#{i}")
+end
+
+(1..10).each do |i|
+  (1..10).each do |m|
+    if i < 5 then
+      Like.create!(user_id: i, post_id: m, suki: 1)
+    else
+      Like.create!(user_id: i, post_id: m, suki: 0)
+    end
+  end
+end
